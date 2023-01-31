dd468caf38ee54a3f74d54f9acea92f3367a7bc3
Carina <checarina@gmail.com>
2023-01-30 21:50:02 -0800

    sketched out CRUD routes for events

--- app/models/Event.py
+++ app/models/Event.py
@@ -8,3 +8,20 @@ class Event(db.Model):
 
     pet_id = db.Column(db.Integer, db.ForeignKey("pet.id"))
     pet = db.relationship("Pet", back_populates = "events")
+
+    @classmethod
+    def from_dict(cls, event_data):
+        new_event = Event(
+            type = event_data["type"],
+            notes = event_data["notes"],
+
+        )
+        return new_event
+    
+    def to_dict(self):
+        event_dict = {}
+        event_dict["id"] = self.id
+        event_dict["type"] = self.type
+        event_dict["timestamp"] = self.timestamp
+
+        return event_dict
--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -1,11 +1,26 @@
 from flask import Blueprint, request, make_response, jsonify
 from app import db
 from app.models.Event import Event
+from app.models.Pet import Pet
 from datetime import datetime
-import requests
-import os
-from dotenv import load_dotenv
+# import requests
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-events_bp = Blueprint("events", __name__, url_prefix = "/events")
+events_bp = Blueprint("events", __name__, url_prefix = "pets/<pet_id>/events")
+
+#CREATE new logged event
+
+#READ all logged events for one pet
+
+#READ logged events for one pet filtered by search query
+#getting ambitious here
+
+#UPDATE specific logged event
+
+#DELETE event
+
+#DELETE all events (for one pet)?
+
